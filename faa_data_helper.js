355f1170560722e63c49d90b606bf2af8df1473b
Sunny Karira <sunnykarira@outlook.com>
2016-06-18 19:02:09 +0530

    Add alexa response to the request of airport status

--- faa_data_helper.js
+++ faa_data_helper.js
@@ -10,6 +10,55 @@
 
 	function FAADataHelper() { }
 
+	FAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
+
+	  var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
+
+	    weather: airportStatus.weather.weather,
+
+	    temp: airportStatus.weather.temp,
+
+	    wind: airportStatus.weather.wind
+
+	  });
+
+	  if (airportStatus.delay === 'true') {
+
+	    var template = _.template('There is currently a delay for ${airport}. ' +
+
+	      'The average delay time is ${delay_time}. ' +
+
+	      'Delay is because of the following: ${delay_reason}. ${weather}');
+
+	    return template({
+
+	      airport: airportStatus.name,
+
+	      delay_time: airportStatus.status.avgDelay,
+
+	      delay_reason: airportStatus.status.reason,
+
+	      weather: weather
+
+	    });
+
+	  } else {
+
+	    //    no delay
+
+	    return _.template('There is currently no delay at ${airport}. ${weather}')({
+
+	      airport: airportStatus.name,
+
+	      weather: weather
+
+	    });
+
+	  }
+
+	};
+
+
 	
 
 	FAADataHelper.prototype.requestAirportStatus = function(airportCode) {
